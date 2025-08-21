6f8fa26b57c3e16334c545a123c2e88142f13251
Frizlab <fload@me.com>
2025-08-22 00:07:49 +0200

    Update LocMapper cask to 1.5

--- Casks/locmapper-app.rb
+++ Casks/locmapper-app.rb
@@ -1,6 +1,6 @@
 cask 'locmapper-app' do
-  version '1.4.0'
-  sha256 'bc54f80097854326405361ec2f90af8e094f9147eddd2a0731f00f8828a52ee5'
+  version '1.5.0'
+  sha256 '38ac4255b97a53d30c29c70921405e6be88fe272839dceadac5bcbd604b1bc7f'
 
   url "https://github.com/xcode-actions/LocMapper/releases/download/LocMapper%2Frelease%2F#{version}/LocMapperApp.zip"
   name 'LocMapper'
